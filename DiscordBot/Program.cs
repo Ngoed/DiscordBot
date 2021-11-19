10a4e452120f4c0df7cd3c85a9918272eac1c6da
ngoed <ngoed@DESKTOPNG>
2021-11-18 20:56:56 -0600

    Add project files.

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscordBot
+{
+	class Program
+	{
+		public static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+	
+
+		private DiscordSocketClient _socketClient;
+		private CommandService _commandService;
+		private IServiceProvider _serviceProvider;		
+
+		public async Task RunBotAsync()
+		{
+			_socketClient = new DiscordSocketClient();
+			_commandService = new CommandService();
+
+			_serviceProvider = new ServiceCollection()
+				.AddSingleton(_socketClient)
+				.AddSingleton(_commandService)
+				.BuildServiceProvider();
+		}
+
+
+	}
+}
