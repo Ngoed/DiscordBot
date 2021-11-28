a47a60cb9c75e393d7aae591da53fcd28c0578af
NathanGoedtke <ngoedtke24@gmail.com>
2021-11-27 19:15:47 -0600

    hw

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -4,6 +4,7 @@ using Discord.WebSocket;
 using Discord.Commands;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace DiscordBot
 {
@@ -25,8 +26,44 @@ namespace DiscordBot
 				.AddSingleton(_socketClient)
 				.AddSingleton(_commandService)
 				.BuildServiceProvider();
+
+			string token = "OTA1MjY1MzY1NDU2NjU4NDQz.YYHkEg.MshFWVIq6TaFq_IBpjPGczYW8Pg";
+
+			_socketClient.Log += _socketClient_Log;
+
+			await RegisterCommandsAsync();
+
+			await _socketClient.LoginAsync(TokenType.Bot, token);
+
+			await _socketClient.StartAsync();
+
+			await Task.Delay(-1);
 		}
 
+		private Task _socketClient_Log(LogMessage arg)
+		{
+			Console.WriteLine(arg);
+			return Task.CompletedTask;
+		}
 
+		public async Task RegisterCommandsAsync()
+		{
+			_socketClient.MessageReceived += HandleCommandAsync;
+			await _commandService.AddModulesAsync(Assembly.GetEntryAssembly(), _serviceProvider);
+		}
+
+		private async Task HandleCommandAsync(SocketMessage arg)
+		{
+			var message = arg as SocketUserMessage;
+			var context = new SocketCommandContext(_socketClient, message);
+			if (message.Author.IsBot) return;
+
+			int argPos = 0;
+			if (message.HasStringPrefix("!",ref argPos ))
+			{
+				var result = await _commandService.ExecuteAsync(context, argPos, _serviceProvider);
+				if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+			}
+		}
 	}
 }
