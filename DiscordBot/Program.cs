17668558ad4a664ea767d25df9e1e94d6e6c9d87
NathanGoedtke <ngoedtke24@gmail.com>
2021-12-12 17:13:17 -0600

    Token

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -37,7 +37,7 @@ namespace DiscordBot
 				//.AddSingleton<LavaConfig>()
 				.BuildServiceProvider();
 			
-			string token = "OTA1MjY1MzY1NDU2NjU4NDQz.YYHkEg.MshFWVIq6TaFq_IBpjPGczYW8Pg";
+			string token = "OTA1MjY1MzY1NDU2NjU4NDQz.YYHkEg.dv4x_H0BeCrLD-uhv76YXmOHOcw";
 
 			_socketClient.Log += _socketClient_Log;
 
